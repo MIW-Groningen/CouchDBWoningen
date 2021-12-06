73ef21efc1982b04def0c80b2ae2d22e15d893ed
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-12-06 15:43:13 +0100

    disabling the setup code disables setting up

--- src/main/java/controller/CouchDBWoningenLauncher.java
+++ src/main/java/controller/CouchDBWoningenLauncher.java
@@ -40,18 +40,18 @@ public class CouchDBWoningenLauncher {
 
 //		Lees de Json-string in het tekstbestand, maak er Verbruiker objecten van en sla die op in CouchDB
 //		Sla alle Verbruikers op in de CouchDb database.
-//		Gson gson = new Gson();
-//		List<Verbruiker> verbruikerList = new ArrayList<>();
-//		try (Scanner bestandLezer = new Scanner(new File("src/main/resources/woningenJson.txt"))) {
-//			while (bestandLezer.hasNext()) {
-//				verbruikerList.add(gson.fromJson(bestandLezer.nextLine(), Verbruiker.class));
-//			}
-//		} catch (FileNotFoundException schrijfFout) {
-//			System.out.println("Het bestand lezen is mislukt.");
-//		}
-//
-//		for (Verbruiker v : verbruikerList) {
-//			cdbf.saveSingleVerbruiker(v);
-//		}
+		Gson gson = new Gson();
+		List<Verbruiker> verbruikerList = new ArrayList<>();
+		try (Scanner bestandLezer = new Scanner(new File("src/main/resources/woningenJson.txt"))) {
+			while (bestandLezer.hasNext()) {
+				verbruikerList.add(gson.fromJson(bestandLezer.nextLine(), Verbruiker.class));
+			}
+		} catch (FileNotFoundException schrijfFout) {
+			System.out.println("Het bestand lezen is mislukt.");
+		}
+
+		for (Verbruiker v : verbruikerList) {
+			cdbf.saveSingleVerbruiker(v);
+		}
 	}
 }
